f437e01260162086d67f1b78d819292806db3d00
Saidou <Saidou@PC-CONTE>
2016-12-19 20:57:50 +0100

    commite du fichier connexion a la base de données du projet test1

---
+++ vues/vue_cours_informatique.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+saidouconte87@gmail.com
---
+++ vues/vue_cours_philosophie.php
@@ -0,0 +1,21 @@
+<div class="jumbotron">
+    <h1>COURS DE MATHS: <small>ISFCE</small></h1>
+</div>
+
+<h1 class="text-center">Un enseignement de pointe</h1>
+
+<div class="row">
+    <p class="col-sm-4">
+         
+    </p>
+    <p class="col-sm-4">
+        
+    </p>
+   
+</div>
+
+<br/>
+
+<img src="./vues/images/presentation.jpg" class="img-responsive img-rounded center-block" style="max-width: 90%">
+
+
