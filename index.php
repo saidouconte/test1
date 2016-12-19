b22a6592d16da918405664d01cef1f39ce0da3f7
vxs <vxs@192.168.10.224>
2016-12-19 20:30:50 +0100

    premier commit ( Jean )

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ and open the template in the editor.
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta charset="UTF-8">
+        <title>Cours 15 DvpWeb</title>
     </head>
     <body>
+        <h1>Projet GIT cours 15 DvpWeb</h1>
+        <p>Lundi 19 Decembre 2016</p>
         <?php
         // put your code here
         ?>
